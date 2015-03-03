b4bd5603441f10b46d466746e8b41dad876ecc3b
Mikael Östman <mikael.ostman@web2life.se>
2015-03-03 16:07:15 +0100

    Added functionality for restricting movement to a region div. Added examples.

--- jQuery.dragmove.js
+++ jQuery.dragmove.js
@@ -4,9 +4,15 @@
 
 (function($) {
 
-    $.fn.dragmove = function() {
+    $.fn.dragmove = function(options) {
     
         return this.each(function() {
+
+            var hasOptions = false;
+
+            if (typeof options !== 'undefined') {
+                hasOptions = true;
+            }
     
             var $document = $(document),
                 $this = $(this),
@@ -15,10 +21,10 @@
                 startY;
             
             $this.on('mousedown touchstart', function(e) {
-            
+                
                 active = true;
                 startX = e.originalEvent.pageX - $this.offset().left;
-                startY = e.originalEvent.pageY - $this.offset().top;	
+                startY = e.originalEvent.pageY - $this.offset().top;
                 
                 if ('mousedown' == e.type)
                     
@@ -30,26 +36,31 @@
                                     
                 if (window.mozInnerScreenX == null)
                 
-                    return false;	
+                    return false;
             });
             
             $document.on('mousemove touchmove', function(e) {
+
+                if (active) {
+                    var newPos = getNewPosition(e);
+                }
                 
                 if ('mousemove' == e.type && active)
-                
-                    click.offset({ 
                     
-                        left: e.originalEvent.pageX - startX,
-                        top: e.originalEvent.pageY - startY 
+                    click.offset({
                     
+                        left: newPos.left,
+                        top: newPos.top
+                        
                     });
+                    
                 
                 if ('touchmove' == e.type && active)
                 
                     touch.offset({
                     
-                        left: e.originalEvent.pageX - startX,
-                        top: e.originalEvent.pageY - startY
+                        left: newPos.left,
+                        top: newPos.top
                         
                     });
                 
@@ -58,6 +69,57 @@
                 active = false;
                 
             });
+
+            function getNewPosition (e) {
+                var newTop;
+                var newLeft;
+                var obj;
+
+                if ('mousemove' == e.type && active) {
+                    obj = click;
+                }
+
+                if ('touchmove' == e.type && active) {
+                    obj = touch;
+                }
+
+                if (hasOptions && typeof options.regionDiv !== 'undefined') {
+                    var maxLeft = options.regionDiv.offset().left;
+                    var maxTop = options.regionDiv.offset().top;
+                    var maxRight = options.regionDiv.offset().left + options.regionDiv.outerWidth();
+                    var maxBottom = options.regionDiv.offset().top + options.regionDiv.outerHeight();
+                    var clickRight = (e.originalEvent.pageX - startX) + obj.width();
+                    var clickBottom = (e.originalEvent.pageY - startY) + obj.height();
+
+                    if (e.originalEvent.pageX - startX > maxLeft) {
+                        newLeft = e.originalEvent.pageX - startX;
+                    } else {
+                        newLeft = maxLeft;
+                    }
+
+                    if (e.originalEvent.pageY - startY > maxTop) {
+                        newTop = e.originalEvent.pageY - startY;
+                    } else {
+                        newTop = maxTop;
+                    }
+
+                    if (clickRight > maxRight) {
+                        newLeft = maxRight - obj.width();
+                    }
+
+                    if (clickBottom > maxBottom) {
+                        newTop = maxBottom - obj.height();
+                    }
+                } else {
+                    newLeft = e.originalEvent.pageX - startX;
+                    newTop = e.originalEvent.pageY - startY;
+                }
+
+                return {
+                    left: newLeft,
+                    top: newTop
+                };
+            }
                                 
         });
             
